4efc5d93b49e317a480f8f35207ea714ed1b9a0c
quswjdgns399 <113302464+quswjdgns399@users.noreply.github.com>
2023-09-16 12:16:54 +0900

    [Bronze V] Title: 문자와 문자열, Time: 212 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -7,9 +7,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
-        int num = sc.nextInt() - 1;
+        int num = sc.nextInt();
 
-        System.out.println(str.charAt(num));
+        System.out.println(str.substring(num - 1, num));
 
 
     }
