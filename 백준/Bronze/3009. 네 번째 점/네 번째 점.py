647a5dd9ba28b8aa1bbcf790767a7ccdfbb47bf5
hoon2 <113302464+quswjdgns399@users.noreply.github.com>
2024-08-19 16:50:19 +0900

    [Bronze III] Title: 네 번째 점, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.py
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -1,25 +1,22 @@
-xArr = [0]*3
-yArr = [0]*3
+xArr = []
+yArr = []
 
-for i in range(0,3):
-  x,y = map(int, input().split())
-  xArr[i] = x
-  yArr[i] = y
+for i in range(3):
+    x, y = map(int, input().split())
+    xArr.append(x)
+    yArr.append(y)
 
 resultX = 0
 resultY = 0
 
-xArr.sort()
-yArr.sort()
+for x in xArr:
+    if xArr.count(x) == 1:
+        resultX = x
+        break
 
-if(xArr[0] == xArr[1]):
-  resultX = xArr[2]
-else:
-  resultX = xArr[0]
-
-if(yArr[0] == yArr[1]):
-  resultY = yArr[2]
-else:
-  resultY = yArr[0]
+for y in yArr:
+    if yArr.count(y) == 1:
+        resultY = y
+        break
 
 print(resultX, resultY)
