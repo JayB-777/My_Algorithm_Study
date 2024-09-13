73bc87fcc38281d0cd6c5d28c7b1547bff5ccc33
hoon2 <quswjdgns3996@gmail.com>
2024-09-13 13:49:43 +0900

    [Silver IV] Title: 카드2, Time: 160 ms, Memory: 59100 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,22 +1,32 @@
 
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Queue<Integer> queue = new LinkedList<>();
-        int n = sc.nextInt();
-        for (int i = 1; i <= n; i++) {
-            queue.add(i);
-        }
+    public static int N;
+    public static Deque<Integer> deque;
 
-        while (queue.size() > 1) {
-            queue.remove();
-            queue.add(queue.poll());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        deque = new ArrayDeque<>(N);
+        for (int i = 1; i <= N; i++) {
+            deque.addLast(i);
         }
-        System.out.println(queue.poll());
+        solve(deque);
+        System.out.println(deque.poll());
+    }
+
+    private static void solve(Deque deque) {
+        if (deque.size() == 1) {
+            return;
+        }
+        deque.poll();
+        deque.addLast(deque.poll());
+        solve(deque);
     }
 }
