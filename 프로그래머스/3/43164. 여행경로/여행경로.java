7cc1f9d66c44a5b89577c6414171f103244daf18
hoon2 <quswjdgns3996@gmail.com>
2024-11-01 21:20:13 +0900

    [level 3] Title: 여행경로, Time: 74.95 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,33 +1,30 @@
 import java.util.*;
 
 class Solution {
-    
-    private List<String> answer;
-    private boolean[] visited;
+    List<String> tripPath;
+    boolean[] visited;
     
     public String[] solution(String[][] tickets) {
-        answer = new ArrayList<>();
+        tripPath = new ArrayList<>();
         visited = new boolean[tickets.length];
         
-        Arrays.sort(tickets, Comparator.comparing((String[] ticket) -> ticket[1]));
+        Arrays.sort(tickets, (a, b) -> a[1].compareTo(b[1]));
         dfs("ICN", "ICN", tickets, 0);
         
-
-        return answer.get(0).split(" ");
+        return tripPath.get(0).split(" ");
         
-
     }
     
     private void dfs(String current, String path, String[][] tickets, int count){
         if(count == tickets.length){
-            answer.add(path);
+            tripPath.add(path);
             return;
         }
         
         for(int i = 0; i < tickets.length; i++){
             if(!visited[i] && tickets[i][0].equals(current)){
                 visited[i] = true;
-                dfs(tickets[i][1], path + " " + tickets[i][1], tickets, count + 1);
+                dfs(tickets[i][1], path + " " + tickets[i][1], tickets, count+1);
                 visited[i] = false;
             }
         }
