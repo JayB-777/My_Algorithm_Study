b7c3c95263cb6c935dd7af7e843718b0ce0e1a12
JayB-777 <quswjdgns3996@gmail.com>
2025-03-10 14:36:31 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1104 ms, Memory: 58700 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -11,44 +11,25 @@ class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         
-//        System.out.println(N + " " +  M);
 
         // 배열입력과 동시에 합배열 생성
-        int[] arr = new int[N];
-        int[] sumArr = new int[N];
-        
+        long[] sumArr = new long[N + 1];
+
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) {
-        	int input = Integer.parseInt(st.nextToken());
-        	
-        	if(i == 0) {
-        		arr[i] = input;
-        		sumArr[i] = input;
-        	} else {
-        		arr[i] = input;
-            	sumArr[i] = sumArr[i - 1] + input;
-        	}
+        for(int i = 1; i <= N; i++) {
+        	sumArr[i] = sumArr[i - 1] + Integer.parseInt(st.nextToken());
         }
         
-//        System.out.println(Arrays.toString(arr));
 //        System.out.println(Arrays.toString(sumArr));
+        
         
         for(int i = 0; i < M; i++) {
         	st = new StringTokenizer(br.readLine());
         	int startIndex = Integer.parseInt(st.nextToken());
         	int endIndex = Integer.parseInt(st.nextToken());
-        	
-        	int result = 0;
-        	
-        	if(startIndex == 1) {
-        		result = sumArr[endIndex - 1];
-        	} else {
-        		result = sumArr[endIndex - 1] - sumArr[startIndex - 2];
-        	}
-        	
+        	long result = sumArr[endIndex] - sumArr[startIndex - 1];
         	System.out.println(result);
         }
-        
     }
 }
 
